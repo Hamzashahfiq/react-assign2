5df35880ad493403b08ce01a0eff2105051b7279
Muhammad hamza <hamzashafique535@gmail.com>
2022-02-22 17:14:25 +0500

    add hero section

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../../components/hero/HeroStyle';
+
+
+function Hero() {
+  return (
+    <div className="row" style={styles.headerBg}>
+        </div>
+  )
+}
+
+
+export default Hero;
---
+++ src/components/hero/HeroStyle.js
@@ -0,0 +1,15 @@
+import Headerimg from '../../assets/images/Heroimg.png';
+
+
+
+const styles = {
+headerBg: {
+    
+    height:"646px",
+    backgroundImage: `url(${Headerimg})`,
+    repeate : "no-repeate",
+    border : '1px solid red'
+  }
+} 
+
+export default styles;
